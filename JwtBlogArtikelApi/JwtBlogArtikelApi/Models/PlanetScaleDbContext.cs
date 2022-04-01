dcb6a46e272f422fd593624e271c76a2f0055908
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2022-04-01 18:13:06 +0200

    web api

---
+++ JwtBlogArtikelApi/JwtBlogArtikelApi/Models/PlanetScaleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JwtBlogArtikelApi.Models
+{
+    public class PlanetScaleDbContext : DbContext
+    {
+        public PlanetScaleDbContext(DbContextOptions<PlanetScaleDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+    }
+}
