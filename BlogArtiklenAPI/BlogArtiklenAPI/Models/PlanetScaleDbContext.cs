cc5c19b420265d048d378f34bbc6f01aeac55f94
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2022-04-01 11:42:10 +0200

    .net core 6 minimal web api

---
+++ BlogArtiklenAPI/BlogArtiklenAPI/Models/PlanetScaleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogArtiklenAPI.Models
+{
+    public class PlanetScaleDbContext : DbContext
+    {
+        public PlanetScaleDbContext(DbContextOptions<PlanetScaleDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+    }
+}
