c5043a92b90d5a7c4ff525b772bb8ff37e8c8a5e
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2022-04-06 13:38:28 +0200

    cleared up null-able errors on models

--- JwtBlogArtikelApi/JwtBlogArtikelApi/Models/UserLike.cs
+++ JwtBlogArtikelApi/JwtBlogArtikelApi/Models/UserLike.cs
@@ -3,10 +3,10 @@
     public class UserLike
     {
         public int Id { get; set; }
-        public string Desciminator { get; set; }
+        public string? Desciminator { get; set; }
         public int OwnerId { get; set; }
         public int LikeId { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
     }
 }
