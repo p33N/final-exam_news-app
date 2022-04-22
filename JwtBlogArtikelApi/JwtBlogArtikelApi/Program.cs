2794cc87fc167210256e8f050d71629314bf5837
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2022-04-22 11:26:43 +0200

    Artikel updatering

--- JwtBlogArtikelApi/JwtBlogArtikelApi/Program.cs
+++ JwtBlogArtikelApi/JwtBlogArtikelApi/Program.cs
@@ -19,11 +19,8 @@ builder.Services.AddDbContextPool<BlogArtiklenDbContext>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
