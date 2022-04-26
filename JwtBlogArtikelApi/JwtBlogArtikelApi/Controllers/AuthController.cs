46bee1d31f20003c87f271e6b09cc6107a896493
ChrSkarvil <christian.skarvil@live.dk>
2022-04-26 10:15:05 +0200

    Auth/Token first version with bugs

---
+++ JwtBlogArtikelApi/JwtBlogArtikelApi/Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using JwtBlogArtikelApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtBlogArtikelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Creating User with a PasswordHash in UserDto
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Email = request.Email;
+            user.passwordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<String>> Login(UserDto request)
+        {
+            if (user.Email != request.Email)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if(!VerifyPasswordHash(request.Password, user.passwordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        // Create Token Function
+        private string CreateToken(User user)
+        {
+            List(Claim) claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings.Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        // Create Password Function
+        private void CreatePasswordHash(String password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        //Verify Password Function
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+    }
+}
--- JwtBlogArtikelApi/JwtBlogArtikelApi/Models/User.cs
+++ JwtBlogArtikelApi/JwtBlogArtikelApi/Models/User.cs
@@ -6,6 +6,9 @@
         public string? Name { get; set; }
         public int EmailId { get; set; }
         public Email? Email { get; set; }
+        public string? Password { get; set; }
+        public byte[]? passwordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
         public string? About { get; set; }
         public bool Subscribtion { get; set; }
         public IList<Follow>? Followers { get; set; }
